8efde513b8a06e3fea428692e251a1445669389a
matheus-reyes <matheus-reyes@usp.br>
2020-06-30 16:29:59 -0300

    Adicionando estrutura do Microprograma

---
+++ Microprograma.java
@@ -0,0 +1,18 @@
+public class Microprograma {
+
+    private String endereco;
+    private String conteudo;
+
+    public Microprograma(String endereco, String conteudo){
+        this.endereco = endereco;
+        this.conteudo = conteudo;
+    }
+
+    public String getEndereco(){
+        return this.endereco;
+    }
+
+    public String getConteudo(){
+        return this.conteudo;
+    }
+}
